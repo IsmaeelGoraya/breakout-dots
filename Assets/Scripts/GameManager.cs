40ece6040aac2b5aaf0152f057a0f748895be071
Ismaeel Shujat <ismaeelshujat@gmail.com>
2021-03-29 23:41:40 +0500

    Refactor GameManager.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -6,11 +5,10 @@ public class GameManager : MonoBehaviour
 	public static GameManager Instance;
 
 	public Vector2 bounds;
+	public Vector2 ScreenSizeInWorldSpace;
 
-	private List<Vector2> newVertices;
-	private EdgeCollider2D edgeCollider;
 
-    private void Awake()
+	private void Awake()
     {
 		if (Instance != null && Instance != this)
 		{
@@ -19,31 +17,17 @@ public class GameManager : MonoBehaviour
 		}
 
 		Instance = this;
+		CalculateScreenBounds();
 	}
 
-    private void Start()
-    {
-		newVertices = new List<Vector2>();
-		edgeCollider = this.GetComponent<EdgeCollider2D>();
-        CreateBoundryEdges();
-        
-    }
-
-    private void CreateBoundryEdges()
+    private void CalculateScreenBounds()
 	{	
 		//Get the boundaries of the camera
 		float camDistance = Vector3.Distance(this.transform.position, Camera.main.transform.position);
 		Vector2 bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, camDistance));
 		Vector2 topRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, camDistance));
         bounds = new Vector2(bottomLeft.x, topRight.y);
-		//Set these to the new vertices for the Edge Collider
-		newVertices.Add (new Vector2(bottomLeft.x, bottomLeft.y)); // Sets the start point
-		newVertices.Add (new Vector2(bottomLeft.x, topRight.y)); // Creates the Top Side
-		newVertices.Add (new Vector2(topRight.x, topRight.y)); // Creates the Right Side
-		newVertices.Add (new Vector2(topRight.x, bottomLeft.y)); // Creates the Bottom Side
-		newVertices.Add (new Vector2(bottomLeft.x, bottomLeft.y)); //Creates the Left Side
-		
-		//Update the edge collider
-		edgeCollider.points = newVertices.ToArray();
+
+		ScreenSizeInWorldSpace = new Vector2(topRight.x * 2, topRight.y * 2);
 	}
 }
